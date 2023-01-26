79bfad94dbf7840591c49bd94ae57da5cdb6d014
mohammed <mohammedfuta@gmail.com>
2023-01-26 23:01:05 +0000

    fifth commit

--- django_proj/urls.py
+++ django_proj/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from http.client import HTTPResponse
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('welcome'),
 ]
+def welcome(request):
+    return HTTPResponse("Welcome!")
